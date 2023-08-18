d8923a8d7a43bc8ec3c89992dfe44a9cd5065110
David Kuehn <kuehnd96@gmail.com>
2023-08-18 11:36:55 -0500

    Implement tests for dependent repository 🧪

--- PaylocityBenefitsCalculator/Api/Models/Dependent.cs
+++ PaylocityBenefitsCalculator/Api/Models/Dependent.cs
@@ -9,4 +9,28 @@ public class Dependent
     public Relationship Relationship { get; set; }
     public int EmployeeId { get; set; }
     public Employee? Employee { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+
+        if (ReferenceEquals(this, obj)) return true;
+
+        if (obj.GetType() != this.GetType()) return false;
+
+        var otherDependent = obj as Dependent;
+
+        if (otherDependent == null) return false;
+
+        return this.Id == otherDependent.Id
+            && this.FirstName == otherDependent.FirstName
+            && this.LastName == otherDependent.LastName
+            && this.DateOfBirth == otherDependent.DateOfBirth
+            && this.Relationship == otherDependent.Relationship
+            && this.EmployeeId == otherDependent.EmployeeId;
+    }
+
+    // NOTE: I should override GetHashCode since I have an override to Equals but
+    //  simply didn't spend the time for this exercise
+    // I would if this was going to production
 }
