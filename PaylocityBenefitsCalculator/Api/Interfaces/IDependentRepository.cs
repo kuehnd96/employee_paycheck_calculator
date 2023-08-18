247f53c08c260acc79a02b95c21f4ce6ec93e7e3
David Kuehn <kuehnd96@gmail.com>
2023-08-18 11:03:51 -0500

    Creation of data repository for dependent data.

---
+++ PaylocityBenefitsCalculator/Api/Interfaces/IDependentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Api.Models;
+
+namespace Api.Interfaces
+{
+    /// <summary>
+    /// Surface for interacting with <see cref="Dependent"/> persisted data.
+    /// </summary>
+    public interface IDependentRepository
+    {
+        // why: Methods return tasks to facilitate implemenations of this interface that are async
+        
+        Task<IEnumerable<Dependent>> GetAll();
+        Task<Dependent?> GetById(int id);
+    }
+}
